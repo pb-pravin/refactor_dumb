a5f027049431c0b31c77988fb25686ff4980eecb
Daniel Lewis <daniel@levelsetlabs.com>
2013-08-26 11:28:45 -0700

    Step 5. Refactor the Controllers -- filters!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+  before_filter :projects_list
+
   def welcome
+  end
+
+  def projects_list
     @projects = Projects.where(active: true)
   end
 end
