1af1ffd844a109df8f164b0a051a0de47f1ed576
Daniel Lewis <daniel@levelsetlabs.com>
2013-08-26 11:29:31 -0700

    Step 6. Refactor into the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :projects_list
+
+  def projects_list
+    @projects = Projects.where(active: true)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,4 @@
 class PagesController < ApplicationController
-  before_filter :projects_list
-
   def welcome
-  end
-
-  def projects_list
-    @projects = Projects.where(active: true)
   end
 end
